b7c9bca9512276cc20797ab5a3e7d11cc3e3ba5a
Hey!Alfred <yu.gao.whut@gmail.com>
2016-05-19 15:32:48 -0700

    BaiduMap test success

--- WHUT_Map/app/src/main/java/com/example/alfredgao/whut_map/MainActivity.java
+++ WHUT_Map/app/src/main/java/com/example/alfredgao/whut_map/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.alfredgao.whut_map;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,12 +14,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.MapView;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SDKInitializer.initialize(getApplicationContext());
+        MapView mMapView = (MapView) findViewById(R.id.bmapView);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -41,6 +47,7 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
+
 
     @Override
     public void onBackPressed() {
@@ -82,6 +89,7 @@ public class MainActivity extends AppCompatActivity
 
         if (id == R.id.nav_mfs) {
             // Handle the camera action
+            Fragment mfsFrag = new mfsFragment();
         } else if (id == R.id.nav_yjt) {
 
         } else if (id == R.id.nav_infolist) {
